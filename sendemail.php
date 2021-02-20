d3afe6a13ca742e466122c710889e9d089a51ed8
Arshyn <arshyn.taizhanov@gmail.com>
2021-02-21 04:31:20 +0600

    pictures for rooms, mail settings for test

--- sendemail.php
+++ sendemail.php
@@ -1,8 +1,8 @@
 <?php
 
 // Define some constants
-define( "RECIPIENT_NAME", "John Doe" );
-define( "RECIPIENT_EMAIL", "youremail@emailservice.com" );
+define( "RECIPIENT_NAME", "Ulan Hot Spring" );
+define( "RECIPIENT_EMAIL", "arshyn.taizhanov@gmail.com" );
 
 
 // Read the form values
@@ -20,12 +20,12 @@ if ( $senderName && $senderEmail && $senderPhone && $message) {
   $success = mail( $recipient, $headers, $msgBody );
 
   //Set Location After Successsfull Submission
-  header('Location: contact.html?message=Successfull');
+  header('Location: index.html?message=Successfull');
 }
 
 else{
 	//Set Location After Unsuccesssfull Submission
-  	header('Location: contact.html?message=Failed');	
+  	header('Location: index.html?message=Failed');	
 }
 
 ?>
